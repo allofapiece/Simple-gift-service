ab885861297de76de30f7cd64e900d5bc2579205
allofapiece <stasana1998@tut.by>
2018-05-07 11:59:18 +0300

    Overrided equals, hashCode and toString methods in entities

--- src/com/epam/entity/EdibleProduct.java
+++ src/com/epam/entity/EdibleProduct.java
@@ -19,4 +19,31 @@ public class EdibleProduct extends Product {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        EdibleProduct that = (EdibleProduct) o;
+
+        if (Float.compare(that.sugar, sugar) != 0) return false;
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (sugar != +0.0f ? Float.floatToIntBits(sugar) : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "sugar = " + sugar + '\n' +
+                "name = " + name + '\n';
+    }
 }
--- src/com/epam/entity/Sweet.java
+++ src/com/epam/entity/Sweet.java
@@ -12,11 +12,26 @@ public class Sweet extends EdibleProduct {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Sweet that = (Sweet) o;
+
+        return type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
-        return "Sweet id = " + getId() + "\n" +
-                "type = " + type + "\n" +
-                "name = " + getName() + " \n" +
-                "weight = " + getWeight() + "\n" +
-                "sugar = " + getSugar() + "\n";
+        return super.toString() +
+                "type = " + type + '\n';
     }
 }
